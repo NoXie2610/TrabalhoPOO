4141c191b6f8cf6b311c5d88f834f71114b725eb
pplace11 <pedro.place@hotmail.com>
2025-01-18 09:32:05 +0000

    atualização

--- src/App.java
+++ src/App.java
@@ -2,8 +2,25 @@ import javax.swing.SwingUtilities;
 
 import Campeonato.Campeonato;
 
-
-
+/**
+ * The App class serves as the entry point for the application.
+ * It initializes a Campeonato object and launches the HomePage GUI.
+ * 
+ * <p>This class contains the main method which is the starting point of the Java application.</p>
+ * 
+ * Usage:
+ * <pre>
+ * {@code
+ * public static void main(String[] args) {
+ *     Campeonato campeonato = new Campeonato();
+ *     SwingUtilities.invokeLater(() -> new HomePage());
+ * }
+ * }
+ * </pre>
+ * 
+ * @see Campeonato
+ * @see HomePage
+ */
 public class App {
     public static void main(String[] args) {
 
